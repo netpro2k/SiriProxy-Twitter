cc644db62ec5b57b51208c412c16598c63cd1b82
netpro2k <netpro2k@gmail.com>
2011-11-27 11:51:59 -0500

    Plugin converted to new API

---
+++ lib/siriproxy-twitter.rb
@@ -0,0 +1,48 @@
+require 'twitter'
+
+class SiriProxy::Plugin::Twitter < SiriProxy::Plugin
+  def initialize(config = {})
+    @config = config 
+    
+    ::Twitter.configure do |config|
+      config.consumer_key = @config['consumer_key'] 
+      config.consumer_secret = @config['consumer_secret']
+      config.oauth_token = @config['oauth_token'] 
+      config.oauth_token_secret = @config['oauth_token_secret']
+    end 
+
+    @twitterClient = ::Twitter::Client.new
+  end
+
+  listen_for /tweet (.+)/i do |tweetText|
+    say "Here is your tweet:"
+
+    # send a "Preview" of the Tweet
+    object = SiriAddViews.new
+    object.make_root(last_ref_id)
+    answer = SiriAnswer.new("Tweet", [
+      SiriAnswerLine.new('logo','http://cl.ly/1l040J1A392n0M1n1g35/content'), # this just makes things looks nice, but is obviously specific to my username
+      SiriAnswerLine.new(tweetText)
+    ])
+    object.views << SiriAnswerSnippet.new([answer])
+    send_object object
+
+    if confirm "Ready to send it?"
+      say "Posting to twitter..."
+      Thread.new {
+        begin
+          @twitterClient.update(tweetText)
+          say "Ok it has been posted."
+        rescue Exception
+          pp $!
+          say "Sorry, I encountered an error: #{$!}"
+        ensure
+          request_completed
+        end
+      }
+    else
+      say "Ok I won't send it."
+      request_completed
+    end
+  end
+end
